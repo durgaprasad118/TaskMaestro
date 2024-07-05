d5c62c63fa1cbda1c562c51f313349e6f6203ed8
Durga Prasad <durgaprasadachana1108@gmail.com>
2024-07-05 16:03:24 +0530

    Added appbar and signin signout buttons

---
+++ components/Appbar.tsx
@@ -0,0 +1,33 @@
+'use client';
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+const Appbar = () => {
+    const session = useSession();
+    return (
+        <div className="flex items-center justify-between p-3">
+            <div>Pro manage</div>
+            {JSON.stringify(session)}
+            {session?.status == 'authenticated' ? (
+                <div className="flex gap-3">
+                    <button
+                        onClick={() => signOut()}
+                        className="px-4 py-2 bg-red-600 rounded  hover:bg-red-800"
+                    >
+                        Sign Out
+                    </button>
+                </div>
+            ) : (
+                <div className="flex gap-3">
+                    <button
+                        onClick={() => signIn()}
+                        className="px-4 py-2 bg-blue-600 rounded  hover:bg-blue-800"
+                    >
+                        Signin
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Appbar;
