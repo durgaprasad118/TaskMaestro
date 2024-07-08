a9c70fe76ebb96cd6c4e78d3530e40547df8e77a
Durga Prasad <durgaprasadachana1108@gmail.com>
2024-07-08 13:47:30 +0530

    server changes

--- app/api/user/page.tsx
+++
@@ -1,8 +0,0 @@
-import { authConfig } from '@/lib/auth';
-import { getServerSession } from 'next-auth';
-const Page = async () => {
-    const session = await getServerSession(authConfig);
-    return <div>{JSON.stringify(session)}</div>;
-};
-
-export default Page;
