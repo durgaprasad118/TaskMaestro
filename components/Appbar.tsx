01987869a04e53898dd9699cbc688228f836c6f0
Durga Prasad <durgaprasadachana1108@gmail.com>
2024-07-08 13:46:14 +0530

    Schema update and appbar showing image

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -6,9 +6,12 @@ const Appbar = () => {
     return (
         <div className="flex items-center justify-between p-3">
             <div>Pro manage</div>
-            {JSON.stringify(session)}
             {session?.status == 'authenticated' ? (
                 <div className="flex gap-3">
+                    <img
+                        className="w-10 h-10 rounded-full"
+                        src={session.data?.user?.image || ''}
+                    />
                     <button
                         onClick={() => signOut()}
                         className="px-4 py-2 bg-red-600 rounded  hover:bg-red-800"
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,48 +1,14 @@
-import CredentialsProvider from 'next-auth/providers/credentials';
+import { db } from '@/db';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GoogleProvider from 'next-auth/providers/google';
-import GitHubProvider from 'next-auth/providers/github';
-
 export const authConfig = {
+    adapter: PrismaAdapter(db),
     providers: [
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                username: { label: 'email', type: 'text', placeholder: '' },
-                password: {
-                    label: 'password',
-                    type: 'password',
-                    placeholder: ''
-                }
-            },
-            async authorize(credentials: any) {
-                console.log(credentials);
-                return {
-                    id: 'user1',
-                    name: 'dp'
-                };
-            }
-        }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || '',
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
-        }),
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID || '',
-            clientSecret: process.env.GITHUB_SECRET || ''
         })
     ],
     secret: process.env.NEXTAUTH_SECRET,
-    callbacks: {
-        session: ({ session, token, user }: any) => {
-            if (session.user) {
-                session.user.id = token.userId;
-            }
-            return session;
-        }
-    }
-    //custom signin page other than the nextauth defualt thingy
-    //just use this
-    // pages: {
-    //     signIn: '/signIN'
-    // }
+    session: { strategy: 'jwt' }
 };
