6f216478a1f2bd907343867d6a96e5ecc7056340
Durga Prasad <durgaprasadachana1108@gmail.com>
2024-07-09 12:32:14 +0530

    Feat: Added animated avatar and made responsive navbar

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -1,37 +1,66 @@
 'use client';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuRadioItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { ResponsiveControl } from '@/layouts/responsive-control';
 import { signIn, signOut, useSession } from 'next-auth/react';
-
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { AnimatedTooltipAvatar } from './ui/animated-tooltip';
 import { Button } from './ui/button';
 const Appbar = () => {
     const session = useSession();
     return (
-        <div className="flex items-center justify-between p-3">
-            <div>Pro manage</div>
-            {session?.status == 'authenticated' ? (
-                <div className="flex gap-3">
-                    <Avatar>
-                        <AvatarImage src={session.data?.user?.image || ''} />
-                        <AvatarFallback>U</AvatarFallback>
-                    </Avatar>
-                    <button
-                        onClick={() => signOut()}
-                        className="px-4 py-2 bg-red-600 rounded  hover:bg-red-800"
-                    >
-                        Sign Out
-                    </button>
+        <header className=" border-b   bg-transparent relative z-10">
+            <ResponsiveControl className="flex flex-row py-2 px-4 h-full items-center justify-between">
+                <div className="header-content-wrapper">
+                    <h1 className=" cursor-pointer relative z-10 text-2xl md:text-4xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+                        Task Maestro
+                    </h1>
                 </div>
-            ) : (
-                <div className="flex gap-3">
-                    <button
-                        onClick={() => signIn()}
-                        className="px-4 py-2 bg-blue-600 rounded  hover:bg-blue-800"
-                    >
-                        Signin
-                    </button>
-                </div>
-            )}
-        </div>
+                {session?.status == 'authenticated' ? (
+                    <div className="mr-8">
+                        <DropdownMenu>
+                            <DropdownMenuTrigger>
+                                <AnimatedTooltipAvatar
+                                    id={1}
+                                    name={session?.data?.user?.name || ''}
+                                    image={session.data?.user?.image || ''}
+                                />
+                            </DropdownMenuTrigger>
+
+                            <DropdownMenuContent className="w-48 text-center">
+                                <DropdownMenuLabel>
+                                    {session.data?.user?.name}
+                                </DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuRadioItem value="Settings">
+                                    Settings
+                                </DropdownMenuRadioItem>
+                                <DropdownMenuRadioItem
+                                    onClick={() => signOut()}
+                                    value="Log Out"
+                                >
+                                    Log Out
+                                </DropdownMenuRadioItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
+                ) : (
+                    <div className="flex gap-3">
+                        <Button
+                            onClick={() => signIn()}
+                            className="bg-blue-600 text-slate-200 hover:text-slate-50    hover:bg-blue-800"
+                        >
+                            Signin
+                        </Button>
+                    </div>
+                )}
+            </ResponsiveControl>
+        </header>
     );
 };
 
---
+++ layouts/page-container.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@/lib/utils';
+import { forwardRef } from 'react';
+
+export interface PageContainerProps
+    extends React.HTMLAttributes<HTMLDivElement> {}
+export const PageContainer = forwardRef<HTMLDivElement, PageContainerProps>(
+    ({ className, ...args }, ref) => {
+        return (
+            <div
+                ref={ref}
+                className={cn('page-container py-6', className)}
+                {...args}
+            />
+        );
+    }
+);
+
+PageContainer.displayName = 'PageContainer';
