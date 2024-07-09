228c82464aa41716cc1d4aaee19acaa4330e57c9
Durga Prasad <durgaprasadachana1108@gmail.com>
2024-07-09 20:01:38 +0530

    Added recoil and atoms

---
+++ store/atoms.ts
@@ -0,0 +1,10 @@
+import { atom, atomFamily, selector, selectorFamily } from 'recoil';
+//
+// export type KanbanBoardContextType = {
+//     kanbanData: KanbanListType[];
+//     setKanbanData: (data: KanbanListType[]) => void;
+//     filteredKanbanData: KanbanListType[];
+//     setFilteredKanbanData: (data: KanbanListType[]) => void;
+//     searchQuery: string;
+//     setSearchQuery: (search: string) => void;
+// };
