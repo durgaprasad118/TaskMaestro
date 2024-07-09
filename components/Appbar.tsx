a9ea97fd1b6944b8843a83473ad30034bec21f73
Durga Prasad <durgaprasadachana1108@gmail.com>
2024-07-09 12:57:45 +0530

    Logo Updated

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -17,9 +17,16 @@ const Appbar = () => {
         <header className=" border-b   bg-transparent relative z-10">
             <ResponsiveControl className="flex flex-row py-2 px-4 h-full items-center justify-between">
                 <div className="header-content-wrapper">
-                    <h1 className=" cursor-pointer relative z-10 text-2xl md:text-4xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
-                        Task Maestro
-                    </h1>
+                    <div className="flex items-center justify-center gap-3">
+                        <img
+                            src="/logo.png"
+                            className="h-12 w-12"
+                            alt="Task Maestro Logo"
+                        />
+                        <h1 className=" cursor-pointer relative z-10 text-2xl md:text-4xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
+                            Task Maestro
+                        </h1>
+                    </div>
                 </div>
                 {session?.status == 'authenticated' ? (
                     <div className="mr-8">
