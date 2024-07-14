6f001cc4330f7dc2735f21cd5d55f633105389c5
Durga Prasad <durgaprasadachana1108@gmail.com>
2024-07-14 23:55:29 +0530

    Added draggable component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Draggable from '@/components/Draggable';
 import { LandingPage } from '@/components/Landing';
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between ">
-            <LandingPage />
+            {/* <LandingPage /> */}
+            <Draggable />
         </main>
     );
 }
