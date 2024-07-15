523989ce0e88b4ba04d91c8f1bef659c51f6ce6f
Durga Prasad <durgaprasadachana1108@gmail.com>
2024-07-15 13:16:26 +0530

    Draggble component added

--- components/DragWithFramer.tsx
+++ components/DragWithFramer.tsx
@@ -1,6 +1,6 @@
 'use client';
-import React, { useRef, useState } from 'react';
 import { motion } from 'framer-motion';
+import { useRef, useState } from 'react';
 
 export const DraggableWithFramer = () => {
     const perDragRef = useRef<number>(0);
