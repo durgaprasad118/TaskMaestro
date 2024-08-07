c6721b0bbeebfdf4697c24bb55e1a9c66d91c296
Durga Prasad <durgaprasadachana1108@gmail.com>
2024-08-07 22:29:42 +0530

    Feat: Calendar view added

---
+++ app/Calendar/page.tsx
@@ -0,0 +1,9 @@
+const View = () => {
+    return (
+        <main className="flex  flex-col items-center justify-between ">
+            <h1>Calednar layout</h1>
+        </main>
+    );
+};
+
+export default View;
