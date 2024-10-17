07c088129162bf77ec48f5cb2c26715448562d74
Durga Prasad <durgaprasadachana1108@gmail.com>
2024-10-17 22:55:36 +0530

    Fix: updated cache-control

--- app/api/backlog/route.ts
+++ app/api/backlog/route.ts
@@ -3,6 +3,7 @@ import { startOfToday } from 'date-fns';
 import { NextResponse } from 'next/server';
 export async function PUT() {
     const today = startOfToday();
+    console.log('function backloggy called');
     try {
         const overdueTasks = await db.task.updateMany({
             where: {
@@ -17,10 +18,15 @@ export async function PUT() {
                 status: 'Backlog'
             }
         });
-
-        return NextResponse.json({
+        const response = NextResponse.json({
             message: `Updated ${overdueTasks.count} tasks to Backlog`
         });
+
+        response.headers.set(
+            'Cache-Control',
+            'no-store, no-cache, must-revalidate, proxy-revalidate'
+        );
+        return response;
     } catch (error) {
         console.error('Error updating overdue tasks:', error);
         return NextResponse.json(
